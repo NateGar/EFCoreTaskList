53e886b9193f1155be11fcf77192411a739de8c7
[Nate] <[ncggardner@gmail.com]>
2020-06-01 00:17:25 -0400

    TaskList EF Core first commit

---
+++ TaskListCapstoneEFCore/Controllers/TasksController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TaskListCapstoneEFCore.Models;
+using Tasks = TaskListCapstoneEFCore.Models.Tasks;
+
+namespace TaskListCapstoneEFCore.Controllers
+{
+    [Authorize]
+    public class TasksController : Controller
+    {
+        private readonly TasksDbContext _context;
+
+        public TasksController(TasksDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult TasksIndex()
+        {
+            string id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var thisUsersTasks = _context.Task.Where(x => x.AspNetUsersId == id).ToList();
+            return View(thisUsersTasks);
+        }
+
+        public IActionResult MarkComplete(int id)
+        {
+            Tasks found = _context.Task.Find(id);
+
+            if (found != null)
+            {
+                //change the things (we're changing the completed status)
+                found.Completed = !found.Completed;
+
+                //modify the state of this entry in the database
+                _context.Entry(found).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                _context.Update(found);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("TasksIndex", new { id = found.Id });
+        }
+
+        public IActionResult DeleteTask(int id)
+        {
+            Tasks found = _context.Task.Find(id);
+            if (found != null)
+            {
+                _context.Task.Remove(found);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("TasksIndex", new { id = found.Id });
+        }
+
+        [HttpGet]
+        public IActionResult AddTask()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddTask(Tasks newTask)
+        {
+            newTask.AspNetUsersId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            if (ModelState.IsValid)
+            {
+                _context.Task.Add(newTask);
+                _context.SaveChanges();
+                return RedirectToAction("TasksIndex");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TaskListCapstoneEFCore/Models/Tasks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TaskListCapstoneEFCore.Models
+{
+    public partial class Tasks
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool? Completed { get; set; }
+        public string AspNetUsersId { get; set; }
+
+        public virtual AspNetUsers AspNetUsers { get; set; }
+    }
+}
